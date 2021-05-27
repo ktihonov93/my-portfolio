951ffc7b378439065fc592578ecd9d0139095583
k_tihonov <k_tihonov@mail.ru>
2021-05-27 03:49:52 +0300

    add buttons for websites

--- src/components/WorkCard.jsx
+++ src/components/WorkCard.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import './WorkCard.css';
 
-export default function WorkCard({title, description, src, link}) {
+export default function WorkCard({title, description, src, link1, link2}) {
     return (
         <div className="WorkCard">
             <img className="WorkCard-img" src={src} alt={title}/>
             <h2>{title}</h2>
             <p className="WorkCard-description">{description}</p>
-            <a href={link}>
+            <a href={link1}>
               <button className="WorkCard-button">
-        view
+        GitHub
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        </button>
+      </a>
+      <a href={link2}>
+              <button className="WorkCard-button">
+        Website
         <span></span>
         <span></span>
         <span></span>
